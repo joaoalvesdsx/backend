b5bd51cb5b21f8c662fe1c378f037fa52e5f8534
Joao <joaoalvesdsx@gmail.com>
2024-06-28 17:16:44 -0300

    c

--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ from database import database
 
 def get_next_sequence_value(sequence_name):
     sequence_document = database.counters.find_one_and_update(
-    {'_id': sequence_name},
-    {'$inc': {'sequence_value': 1}},
-    return_document=True,
-    upsert=True
+        {'_id': sequence_name},
+        {'$inc': {'sequence_value': 1}},
+        return_document=True,
+        upsert=True
     )
     return sequence_document['sequence_value']
 
