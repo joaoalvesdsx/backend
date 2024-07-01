45bd92fa08bd72bf498c6a814d45c30df1238809
Joao <joaoalvesdsx@gmail.com>
2024-07-01 00:32:13 -0300

    asd

--- database.py
+++ database.py
@@ -30,60 +30,6 @@ class Database:
     
     def initialize_database(self):
         db= self.db
-        # Verificar se a coleção 'counters' existe e criar se não existir
-        if 'counters' not in db.list_collection_names():
-            db.create_collection('counters')
-
-        # Verificar e inicializar o contador para 'empresa_id'
-        if db.counters.find_one({"_id": "empresa_id"}) is None:
-            db.counters.insert_one({
-            "_id": "empresa_id",
-            "seq": 0
-        })
-
-        # Verificar e inicializar o contador para 'contato_id'
-        if db.counters.find_one({"_id": "contato_id"}) is None:
-            db.counters.insert_one({
-            "_id": "contato_id",
-            "seq": 0
-        })
-
-        # Verificar e inicializar o contador para 'proposta_id'
-        if db.counters.find_one({"_id": "proposta_id"}) is None:
-            db.counters.insert_one({
-            "_id": "proposta_id",
-            "seq": 0
-        })
-
-    # Verificar e inicializar o contador para 'visita_id'
-        if db.counters.find_one({"_id": "visita_id"}) is None:
-            db.counters.insert_one({
-            "_id": "visita_id",
-            "seq": 0
-        })
-
-    # Verificar e inicializar o contador para 'imagem_id'
-        if db.counters.find_one({"_id": "imagem_id"}) is None:
-            db.counters.insert_one({
-            "_id": "imagem_id",
-            "seq": 0
-        })
-
-    # Verificar e inicializar o contador para 'revisao_id'
-        if db.counters.find_one({"_id": "revisao_id"}) is None:
-            db.counters.insert_one({
-            "_id": "revisao_id",
-            "seq": 0
-        })
-
-    # Verificar e inicializar o contador para 'tratativa_id'
-        if db.counters.find_one({"_id": "tratativa_id"}) is None:
-            db.counters.insert_one({
-            "_id": "tratativa_id",
-            "seq": 0
-        })
-
-    # Inicializar coleções para cada entidade
         collections = ['empresas', 'contatos', 'propostas', 'visitas', 'imagens', 'revisoes', 'tratativas']
         for collection in collections:
             if collection not in db.list_collection_names():
